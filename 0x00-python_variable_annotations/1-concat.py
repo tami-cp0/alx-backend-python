eb648723aa3c86b9cac4396c1a46a39c438ea2d2
tami-cp0 <findtamilore>
2024-06-07 17:38:29 +0100

    task 1 done

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module to concatenate two strings and show type annotation.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings
+
+    Returns:
+        The concatenated string.
+    """
+
+    return str1 + str2
