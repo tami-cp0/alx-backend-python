f27844f88840551dd839d1061cc3741fe2583733
tami-cp0 <findtamilore>
2024-06-07 17:43:57 +0100

    task

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module that implements the to_str function
+and shows type annotations.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns:
+        The string representation of a float.
+    """
+    return str(n)
