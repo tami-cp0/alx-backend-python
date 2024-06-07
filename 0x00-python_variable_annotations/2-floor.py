e02ea92835054d7a041f5d48d1a452dc15df8653
tami-cp0 <findtamilore>
2024-06-07 17:41:41 +0100

    task 2 done

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module to return the floor of a float num
+and show type annotations.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns:
+        The floor of a float number.
+    """
+    return int(n)
