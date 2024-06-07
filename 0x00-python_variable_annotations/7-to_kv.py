628284586ecc0d9e45c214ba541d0c2e5d56ccf0
tami-cp0 <findtamilore>
2024-06-07 18:30:37 +0100

    test

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""
-Module to showcase type annotations
-"""
-from typing import Tuple, Union
+""" Module that perform an operation to tuple"""
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+import typing
+
+
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
     """returns a tuple of the string & square of v as float"""
     return (k, float(v * v))
