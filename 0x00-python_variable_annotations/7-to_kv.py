46255ee1fe7591fb57d00f3abc8e00920a1ad1d4
tami-cp0 <findtamilore>
2024-06-07 18:25:43 +0100

    corrected annotation

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ Module to showcase type annotations
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[float, int]) -> tuple:
+def to_kv(k: str, v: Union[float, int]) -> tuple[str, float]:
     """
     Returns:
         A tuple:
