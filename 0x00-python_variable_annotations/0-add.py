7ab1dde23a843189e8983f96b2349a963ae007ea
tami-cp0 <findtamilore>
2024-06-07 17:20:10 +0100

    task 0 done

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module to add two floating-point numbers.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floating-point numbers.
+
+    Args:
+        a (float): First number.
+        b (float): Second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
