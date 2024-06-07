8b59cc119e6ebd5404a10923d983a33d64d4c26d
tami-cp0 <findtamilore>
2024-06-07 17:52:43 +0100

    task 5 done

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Adds all the floats in input list
+
+    Returns:
+        Sum of the floats in the input_list
+    """
+    return sum(input_list)
